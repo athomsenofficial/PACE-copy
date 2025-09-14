baf2b5fdbdfee3eb3a82e88ea621a1ea0d68fd3b
athomsenofficial <113614410+athomsenofficial@users.noreply.github.com>
2025-09-14 17:48:07 +0900

    Update accounting_date_check.py
    corrected bug in accounting date check

--- accounting_date_check.py
+++ accounting_date_check.py
@@ -6,7 +6,7 @@ def accounting_date_check(date_arrived_station, grade, year):
     if not date_arrived_station:
         return False
 
-    scod = f'{SCODS.get(grade)}-{year + 1}'
+    scod = f'{SCODS.get(grade)}-{year}'
     formatted_scod_date = datetime.strptime(scod, "%d-%b-%Y")
     accounting_date = formatted_scod_date - relativedelta(days=120 - 1)
     adjusted_accounting_date = accounting_date.replace(day=3, hour=23, minute=59, second=59)
